31f9cde29468a1843d2ae8ed993cd95decab6014
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-29 11:02:40 +0700

    Add mini project 14

---
+++ 14_BMI-Calculator/bmi-calculator.py
@@ -0,0 +1,23 @@
+print('*'*48)
+print("CALCULATE BODY MASS INDEX (BMI)")
+print('*'*48)
+
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+
+BMI = weight / (height/100)**2
+
+print(f"\nYour BMI is {BMI}")
+
+if BMI <= 18.4:
+    print("You are underweight.")
+elif BMI <= 24.9:
+    print("You are healthy.")
+elif BMI <= 29.9:
+    print("You are overweight.")
+elif BMI <= 34.9:
+    print("You are severely overweight.")
+elif BMI <= 39.9:
+    print("You are obese.")
+else:
+    print("You are severely obese.")
